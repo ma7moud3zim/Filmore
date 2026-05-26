ac73201f742591a2c8233227d0c35c732dc5f318
Mahmoud Abdelazim <eng3zim@gmail.com>
2026-05-26 03:45:02 +0300

    Initialized the project
    Initialized the project using Spring Initializr service.

---
+++ filmore/src/main/java/com/azim/filmore/FilmoreApplication.java
@@ -0,0 +1,13 @@
+package com.azim.filmore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmoreApplication.class, args);
+	}
+
+}
---
+++ filmore/src/test/java/com/azim/filmore/FilmoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azim.filmore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
