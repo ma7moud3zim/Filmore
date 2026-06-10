1848dff69afad553b64974223632982f70eb133e
Mahmoud Abdelazim <eng3zim@gmail.com>
2026-06-10 14:36:59 +0300

    Added the Sahred_Module and added material modules to it

--- filmorefe/src/app/app-module.ts
+++ filmorefe/src/app/app-module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Landing } from './landing/landing';
+import { SharedModule } from './shared/shared-module';
 
 @NgModule({
   declarations: [App, Landing],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, SharedModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
 })
--- filmorefe/src/app/app-routing-module.ts
+++ filmorefe/src/app/app-routing-module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Landing } from './landing/landing';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: Landing },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
