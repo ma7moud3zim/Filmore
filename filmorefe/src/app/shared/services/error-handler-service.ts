464f03935faecee7c31c0b817403f41284ae2bbc
Mahmoud Abdelazim <eng3zim@gmail.com>
2026-06-13 03:06:13 +0300

    Wrote notification service code

--- filmorefe/src/app/shared/services/error-handler-service.ts
+++ filmorefe/src/app/shared/services/error-handler-service.ts
@@ -1,6 +1,16 @@
 import { Injectable } from '@angular/core';
+import { NotificationService } from './notification-service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ErrorHandlerService {}
+export class ErrorHandlerService {
+  constructor(private notification: NotificationService) {}
+
+  // This method is used to handle errors and display a notification to the user.
+  handle(error: any, fullbackMessage: string) {
+    const errorMsg = error.error?.error || fullbackMessage;
+    this.notification.error(errorMsg);
+    console.error(error);
+  }
+}
