bb3f69b4c206eff1b017fda77736484dd8860405
Mahmoud Abdelazim <eng3zim@gmail.com>
2026-06-13 03:02:54 +0300

    Wrote notification Service code

--- filmorefe/src/app/shared/services/notification-service.ts
+++ filmorefe/src/app/shared/services/notification-service.ts
@@ -1,6 +1,27 @@
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root',
 })
-export class NotificationService {}
+export class NotificationService {
+  constructor(private snackBar: MatSnackBar) {}
+
+  sucess(message: string, duration: number = 4000) {
+    this.snackBar.open(message, 'Close', {
+      duration: duration,
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+      panelClass: ['notification-success'],
+    });
+  }
+
+  error(message: string, duration: number = 4000) {
+    this.snackBar.open(message, 'Close', {
+      duration: duration,
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+      panelClass: ['notification-error'],
+    });
+  }
+}
