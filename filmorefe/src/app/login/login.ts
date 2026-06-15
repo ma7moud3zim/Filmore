971be935ec6a84b8e6f80e0b0577de288b909272
Mahmoud Abdelazim <eng3zim@gmail.com>
2026-06-15 07:58:06 +0300

    Created the login page
    Created the page and tested it with verification also.

--- filmorefe/src/app/login/login.ts
+++ filmorefe/src/app/login/login.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { ErrorHandlerService } from './../shared/services/error-handler-service';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../shared/services/auth-service';
+import { NotificationService } from '../shared/services/notification-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,4 +11,82 @@ import { Component } from '@angular/core';
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login implements OnInit {
+  hide = true;
+  loginForm!: FormGroup;
+  loading = false;
+  showResendLink = false;
+  userEmail = '';
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+    private notification: NotificationService,
+    private errorHandlerService: ErrorHandlerService,
+  ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  ngOnInit(): void {
+    if (this.authService.isLoggedIn()) {
+      this.authService.redirectBasedOnRole();
+    }
+  }
+
+  submit() {
+    this.loading = true;
+    const formData = this.loginForm.value;
+    const authData = {
+      email: formData.email?.trim().toLowerCase(),
+      password: formData.password,
+    };
+    this.authService.login(authData).subscribe({
+      next: (response: any) => {
+        this.loading = false;
+        this.authService.redirectBasedOnRole();
+      },
+      error: (err) => {
+        this.loading = false;
+        const errorMsg = err.error?.error || 'Login failed. Please, check your credentials.';
+        if (err.status === 403 && errorMsg.toLowerCase().includes('verified')) {
+          this.showResendLink = true;
+          this.userEmail = this.loginForm.value.email;
+        } else {
+          this.showResendLink = false;
+        }
+        this.notification.error(errorMsg);
+        console.error('Login error:', err);
+      },
+    });
+  }
+
+  resendVerification() {
+    if (!this.userEmail) {
+      this.notification.error('Please enter your email');
+      return;
+    }
+    this.showResendLink = false;
+    this.loading = true;
+    this.authService.resendVerificationEmail(this.userEmail).subscribe({
+      next: (response: any) => {
+        this.loading = false;
+        this.notification.sucess(response?.message || 'Verification email sent successfully.');
+      },
+      error: (err) => {
+        this.loading = false;
+        this.errorHandlerService.handle(
+          err,
+          'Failed to send verification email. Please try again.',
+        );
+      },
+    });
+  }
+
+  forgot() {
+    this.router.navigate(['/forget-password']);
+  }
+}
--- filmorefe/src/app/shared/services/auth-service.ts
+++ filmorefe/src/app/shared/services/auth-service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+import { BehaviorSubject, tap } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
@@ -9,7 +11,13 @@ import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 export class AuthService {
   private apiUrl = environment.apiUrl + '/auth';
 
-  constructor(private http: HttpClient) {}
+  private currentUserSubject = new BehaviorSubject<any>(null);
+  public currentUser$ = this.currentUserSubject.asObservable();
+
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+  ) {}
 
   passwordMatchValidator(passwordControlName: string): ValidatorFn {
     return (confirmControl: AbstractControl): ValidationErrors | null => {
@@ -37,5 +45,62 @@ export class AuthService {
   // and sends a GET request to the /verify-email endpoint of the API with the token as a query parameter.
   verifyEmail(token: string) {
     return this.http.get(this.apiUrl + '/verify-email?token=' + token);
+  }
+
+  // a method takes login data and sends a Post request to the /login endpoint of the API
+  // and returns the observable from the HTTP request
+  login(loginData: any) {
+    return this.http
+      .post(`${this.apiUrl}/login`, loginData)
+      .pipe(tap((response) => this.handleAuthSuccess(response)));
+  }
+
+  handleAuthSuccess(authData: any) {
+    if (authData?.token) {
+      localStorage.setItem('token', authData.token);
+    }
+    this.setCurrentUser(authData);
+  }
+
+  logout() {
+    this.currentUserSubject.next(null);
+  }
+
+  setCurrentUser(user: any) {
+    this.currentUserSubject.next(user);
+  }
+
+  getCurrentUser() {
+    return this.currentUserSubject.value;
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('token');
+  }
+
+  isAuthenticated() {
+    return !!this.getToken();
+  }
+
+  getRole() {
+    const user = this.getCurrentUser();
+    return user?.role;
+  }
+
+  isLoggedIn(): Boolean {
+    return !!this.getToken();
+  }
+  redirectBasedOnRole() {
+    const targetUrl = this.isAdmin() ? '/admin' : '/home';
+    this.router.navigate([targetUrl]);
+  }
+
+  isAdmin(): boolean {
+    const user = this.getCurrentUser();
+    return user?.role === 'ADMIN';
+  }
+
+  resendVerificationEmail(email: string) {
+    return this.http.post(this.apiUrl + '/resend-verification', { email });
   }
 }
