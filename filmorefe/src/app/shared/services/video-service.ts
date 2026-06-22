5dc8ab418afb2b6444e4547b9f4950fb0623fb7c
Mahmoud Abdelazim <eng3zim@gmail.com>
2026-06-22 22:03:56 +0300

    Wrote code of videoService

--- filmorefe/src/app/shared/services/video-service.ts
+++ filmorefe/src/app/shared/services/video-service.ts
@@ -1,6 +1,52 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
-export class VideoService {}
+export class VideoService {
+  private apiUrl = environment.apiUrl + '/videos';
+  private apiUrlAdmin = environment.apiUrl + '/videos/admin';
+
+  constructor(private http: HttpClient) {}
+  getAllAdminVideos(page: number, size: number, search?: string) {
+    let params = new HttpParams().set('page', page).set('size', size);
+    if (search) {
+      params = params.set('search', search);
+    }
+    return this.http.get(this.apiUrlAdmin, { params });
+  }
+
+  createVideoByAdmin(data: any) {
+    return this.http.post(this.apiUrlAdmin, data);
+  }
+
+  updateVideoByAdmin(id: string | number, data: any) {
+    return this.http.patch(this.apiUrlAdmin + '/' + id, data);
+  }
+
+  deleteVideoByAdmin(id: string | number) {
+    return this.http.delete(this.apiUrlAdmin + '/' + id);
+  }
+
+  setPublishedByAdmin(id: string | number, published: boolean) {
+    return this.http.patch(this.apiUrlAdmin + '/' + id, '/publishe?value=' + published, {});
+  }
+
+  getStatsByAdmin() {
+    return this.http.get(this.apiUrlAdmin + '/stats');
+  }
+
+  getPublishedVideosPaginated(page: number = 0, size: number = 10, search?: string) {
+    let params = new HttpParams().set('page', page).set('size', size);
+    if (search) {
+      params = params.set('search', search);
+    }
+    return this.http.get(this.apiUrl + '/published', { params });
+  }
+
+  getFeaturedVideos() {
+    return this.http.get(this.apiUrl + '/featured');
+  }
+}
