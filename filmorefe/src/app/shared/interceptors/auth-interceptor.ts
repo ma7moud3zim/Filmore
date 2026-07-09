5b58f2c2a04cf98f105f84e3484087fb541579c4
Mahmoud Abdelazim <eng3zim@gmail.com>
2026-07-09 03:00:35 +0300

    Fixed bug in intercecptor

--- filmorefe/src/app/shared/interceptors/auth-interceptor.ts
+++ filmorefe/src/app/shared/interceptors/auth-interceptor.ts
@@ -1,14 +1,28 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { AuthService } from '../services/auth-service';
+import { catchError, throwError } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(AuthService);
   const token = localStorage.getItem('token');
+
+  let request = req;
+
   if (token) {
-    const cloned = req.clone({
+    request = req.clone({
       setHeaders: {
         Authorization: `Bearer ${token}`,
       },
     });
-    return next(cloned);
-  }
-  return next(req);
+  } else console.log('No token found, request sent without auth');
+
+  return next(request).pipe(
+    catchError((error) => {
+      if (error.status === 401 || error.status === 403) {
+        authService.logout();
+      }
+      return throwError(() => error);
+    }),
+  );
 };
